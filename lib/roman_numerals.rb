af135093184a814419e35293ed8cd51a7cbbed31
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-07-03 22:36:26 +0100

    method improved

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,9 +1,15 @@
 class RomanNumerals
 
-  ROMAN_NUMERALS = {10 => "X", 5 => "V", 1 => "I",}
+  ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I",}
   
   def self.convert(number)
-    ROMAN_NUMERALS[number]
+    roman_numeral = ""
+    ROMAN_NUMERALS.each do |key,value|
+      (number / key).times {roman_numeral << value; number -= key}
+    end
+    roman_numeral
+
+
   end
 
 end
